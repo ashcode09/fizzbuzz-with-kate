42b2f6986f81eae563362f9c2e42167cabf09c60
Ashleigh <ashleighmaund@gmail.com>
2015-06-02 18:02:36 +0100

    added unit test - just returns number when passed number not divisible by 3 or 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,16 +2,20 @@ require 'fizzbuzz'
 
 describe "Fizzbuzz" do
 	
-	it 'to return "fizz!" when given 3 as argument' do
+	it 'to return "fizz!" when given 9 as argument' do
 		expect(fizzbuzz(9)).to eq 'fizz!'
 	end
 
-	it 'to return "buzz!" when given 5 as argument' do
+	it 'to return "buzz!" when given 25 as argument' do
 		expect(fizzbuzz(25)).to eq 'buzz!'
 	end
 
-	it 'to return "fizzbuzz!" when given 15 as argument' do
+	it 'to return "fizzbuzz!" when given 60 as argument' do
 		expect(fizzbuzz(60)).to eq 'fizzbuzz!'
+	end
+
+	it 'to return 7 when given 7 as argument' do
+		expect(fizzbuzz(7)).to eq 7
 	end
 
 end
